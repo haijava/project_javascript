9ea98ed26d1274ca7b598c1989e3af27d897763a
= <=>
2019-09-05 14:44:39 +0700

    declare

---
+++ Thuchanhjavascript/Declare_and_use_variables/baitap2.js
@@ -0,0 +1,6 @@
+function areRec(){
+	let width = 20;
+	let height =10;
+	let area= width*height;
+	document.write("AreRec is:"+ area);
+}
---
+++ Thuchanhjavascript/Declare_and_use_variables/baitap3.js
@@ -0,0 +1,11 @@
+function Boiso() {
+	let a;
+	let b;
+	a=prompt("Enter a=");
+	b=prompt("Enter b=");
+	if (a%b==0) {
+		alert('a là bội số của b');
+	}
+	else 
+		alert('a không phải là bội số của b');
+}
