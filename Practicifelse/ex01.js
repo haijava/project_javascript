9cd73f638fba66b375cf306318a0ff146bca8409
= <=>
2019-09-09 09:15:49 +0700

    ifelse

---
+++ Practicifelse/ex01.js
@@ -0,0 +1,10 @@
+function praIfEl() {
+	let value=prompt('What is the "official"name of JavaScript?');
+		if (value=='ECMAScript') {
+			alert('Right')
+		} else
+			{
+				alert('Didn’t know? ECMAScript!');
+	        }
+
+	}
