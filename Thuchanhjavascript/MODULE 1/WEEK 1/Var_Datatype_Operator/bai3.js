d0befdfdc97f2def13435d42a3a22ac2e710f7e5
= <=>
2019-09-11 22:32:33 +0700

    week1

--- Thuchanhjavascript/MODULE 1/WEEK 1/Var_Datatype_Operator/bai3.js
+++ Thuchanhjavascript/MODULE 1/WEEK 1/Var_Datatype_Operator/bai3.js
@@ -1,6 +1,6 @@
 function areaCircle() {
 	let radiCir = prompt("Nhập Bán kính hình tròn");
-	/*const piCir= 3.14;*/
-	let areaC = radiCir*radiCir;
+	const piCir= 3.14;
+	let areaC = radiCir*radiCir*piCir;
 	document.write("Diện tích hình  tròn là: "+ areaC);
 }
